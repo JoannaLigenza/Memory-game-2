c3d443858e24fd1334c9753f3ecdbb52874c5b13
Joanna Ligenza <39116583+JoannaLigenza@users.noreply.github.com>
2018-06-29 17:27:52 +0200

    New version
    Version with one object

--- script.js
+++ script.js
@@ -1,141 +1,137 @@
 
 document.addEventListener('DOMContentLoaded', function() {
+	
 	const gameContainer = document.getElementById("gameContainer");
 	const allTd = document.getElementsByTagName("td");	
-	const colorsArr = ["red", "blue", "green", "yellow", "orange", "rose", "lila", "sea", "green2", 
-						"pink", "purple", "creme", "gray", "bronze", "green3"];
-	const clicked = [];
-	const clickNumber = [];
-	const invisible = [];
 	
-	// Draw table
-	function drawTable(x,y) {
-		const table = document.createElement("table");
-		table.id = "table";
-		gameContainer.appendChild(table);
-		for (let i=1; i <= y; i++) {
-					const tr = table.appendChild(document.createElement("tr"));
-						for (let j=1; j <= x; j++) {
-							const td = tr.appendChild(document.createElement("td"));
-							td.classList.add("tds");
-							td.classList.add("front");
-							const tdsInside = td.appendChild(document.createElement("div"));
-							tdsInside.classList.add("tds-inside");
-						}
-		}
-		randomColor(x, y);
-	}
+	const game = { 
 	
-	// Allows to change size of array with colors. Max array size: 6x5
-	function randomTab(z) {
-		let temp = colorsArr.slice(0,z).concat(colorsArr.slice(0,z));
-		return temp;
-	}
-	
-	// Set random color to every td
-	function randomColor(x, y) {
-		const arr = randomTab((x*y)/2);
-		for (let i=0; i < allTd.length; i++ ) {
-				let randomColor = Math.floor(Math.random() * arr.length);
-				const addColor = allTd[i].classList.add(arr[randomColor]);
-				var color = arr[randomColor];			// musze uzyc var zeby color zostal zlapany przez wywolywana ponizej funkcje eventListener(color); // Let nie dziala
-				arr.splice(randomColor, 1);
-		} 
-		eventListener(color);
-	}
-	
-	// Check which td was clicked
-	function eventListener(color) { 
-	for (let i=0; i < allTd.length; i++) { 
-		allTd[i].addEventListener("click", function(){
-			toggleClass(allTd[i], color);
-		});
-	}}
-		
-	// Show and hide colors when click on td
-	function toggleClass(td, color) {
-		// Block click on invisible colors
-		if (td.classList.contains("invisible")) {
-			return;
-		}
-		clickNumber.push("1");
-		// Set clickNumber to 1 if the same td was clicked
-		if (clickNumber.length > 2 && clicked.length < 2) {
-			clickNumber.length = 1;		
-		}
-		// This eliminates errors :P
-		if (clicked.length > 0 && clickNumber.length < 2) {
-			return;
-		}
-		// Check click number - block more than two click
-		if (clickNumber.length > 2) {
-			return;
-		} 
-		// Show colors
-		if (clicked.length < 2) { 
-			if (td.classList.contains("front")) {
-				td.classList.remove("front");
-				clicked.push(td);
+		colorsArr: ["red", "blue", "green", "yellow", "orange", "rose", "lila", "sea", "green2", 
+							"pink", "purple", "creme", "gray", "bronze", "green3"],
+		clicked: [],
+		clickNumber: [],
+		invisible: [],
+		 
+		// Draw table
+		drawTable: function(x,y) {
+			const table = document.createElement("table");
+			table.id = "table";
+			gameContainer.appendChild(table);
+			for (let i=1; i <= y; i++) {
+						const tr = table.appendChild(document.createElement("tr"));
+							for (let j=1; j <= x; j++) {
+								const td = tr.appendChild(document.createElement("td"));
+								td.classList.add("tds");
+								td.classList.add("front");
+								const tdsInside = td.appendChild(document.createElement("div"));
+								tdsInside.classList.add("tds-inside");
+							}
 			}
-		} 
-		// Hide colors
-		if (clicked.length == 2) {
-			setTimeout(function() {
-				sleep(clicked, td)
-			}, 800);
-		}
-	};  
-	
-	// Wait 1 sec before hide colors
-	function sleep(clicked, td) {
-		if (clicked[0].className == clicked[1].className) {
-			for (let i=0; i < 2; i++) {
-				clicked[i].classList.add("invisible");
-				clicked[i].classList.add("front");
-				invisible.push(1);
-				
-				// Check result
-				if (invisible.length == allTd.length) {
-					showWin("WYGRANA!");
+			game.setRandomColor(x, y);
+		},
+		
+		// Allows to change size of array with colors. Max array size: 6x5
+		randomTab: function(z) {
+			let temp = this.colorsArr.slice(0,z).concat(this.colorsArr.slice(0,z));
+			return temp;
+		},
+		
+		// Set random color to every td
+		setRandomColor: function(x, y) {
+			const arr = game.randomTab((x*y)/2);
+			for (let i=0; i < allTd.length; i++ ) {
+					let randomColor = Math.floor(Math.random() * arr.length);
+					const addColor = allTd[i].classList.add(arr[randomColor]);
+					var color = arr[randomColor];
+					arr.splice(randomColor, 1);
+			} 
+			game.eventListener(color);
+		},
+		
+		// Check which td was clicked
+		eventListener: function(color) { 
+			for (let i=0; i < allTd.length; i++) { 
+				allTd[i].addEventListener("click", function(){
+					game.toggleClass(allTd[i], color);
+				});
+			}	
+		},
+			
+		// Show and hide colors when click on td
+		toggleClass: function(td, color) {
+			// Block click on invisible colors
+			if (td.classList.contains("invisible")) {
+				return;
+			}
+			this.clickNumber.push("1");
+			// Set clickNumber to 1 if the same td was clicked
+			if (this.clickNumber.length > 2 && this.clicked.length < 2) {
+				this.clickNumber.length = 1;		
+			}
+			// This eliminates errors :P
+			if (this.clicked.length > 0 && this.clickNumber.length < 2) {
+				return;
+			}
+			// Check click number - block more than two click
+			if (this.clickNumber.length > 2) {
+				return;
+			} 
+			// Show colors
+			if (this.clicked.length < 2) { 
+				if (td.classList.contains("front")) {
+					td.classList.remove("front");
+					this.clicked.push(td);
+				}
+			} 
+			// Hide colors
+			if (this.clicked.length == 2) {
+				setTimeout(function() {
+					game.sleep(this.clicked, td)
+				}, 800);
+			}
+		},
+		
+		// Wait 1 sec before hide colors
+		sleep: function(clicked, td) {
+			if (this.clicked[0].className == this.clicked[1].className) {
+				for (let i=0; i < 2; i++) {
+					this.clicked[i].classList.add("invisible");
+					this.clicked[i].classList.add("front");
+					this.invisible.push(1);
+					
+					// Check result
+					if (this.invisible.length == allTd.length) {
+						game.showWin("WYGRANA!");
+					}
 				}
 			}
-		}
-		for (let i=0; i < 2; i++) {
-			clicked[i].classList.add("front");
-		}
-		clicked.splice(0, clicked.length);
-		clickNumber.splice(0,clickNumber.length);
-	}
-	
-	function showWin(textt){
-		const result = document.createElement("div");
-		const h1 = document.createElement("h2");
-		result.id = "result";
-		gameContainer.appendChild(result);
-		result.appendChild(h1);
-		h1.textContent = textt;
-		const buton = document.createElement("button");
-		buton.id = "button";
-		buton.textContent = "Graj ponownie";
-		result.appendChild(buton);
-		resetGame(buton);
-	}
-	
-	function resetGame(buton) {
-		buton.addEventListener("click", function() {
-			location.reload();
-		})
-		/*this.addEventListener("click", function() {
-			for (let i=0; i < allTd.length; i++) { 
-				allTd[i].classList.remove("invisible");
+			for (let i=0; i < 2; i++) {
+				this.clicked[i].classList.add("front");
 			}
-			clicked.splice(0, clicked.length);
-			clickNumber.splice(0,clickNumber.length);
-			//clicked.splice(0, clicked.length);
-			gameContainer.innerText = "";
-			drawTable(2,3);  
-		}  ) */
+			this.clicked.splice(0, this.clicked.length);
+			this.clickNumber.splice(0, this.clickNumber.length);
+		},
+		
+		showWin: function(textt){
+			const result = document.createElement("div");
+			const h1 = document.createElement("h2");
+			result.id = "result";
+			gameContainer.appendChild(result);
+			result.appendChild(h1);
+			h1.textContent = textt;
+			const buton = document.createElement("button");
+			buton.id = "button";
+			buton.textContent = "Graj ponownie";
+			result.appendChild(buton);
+			game.resetGame(buton);
+		},
+		
+		resetGame: function(buton) {
+			buton.addEventListener("click", function() {
+				location.reload();
+			})
+		}
 	}
 	
-	drawTable(4,3);
+	game.drawTable(4,3);
 });
